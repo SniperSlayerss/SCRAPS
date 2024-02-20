3ed14a094156c379897c834f7decb9835df444f1
James Medley <81381425+jmed13@users.noreply.github.com>
2024-02-20 17:03:49 +0000

    basic code layout

---
+++ app/src/main/java/com/example/scraps/FoodItemScreen.java
@@ -0,0 +1,14 @@
+package com.example.scraps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FoodItemScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_item_screen);
+    }
+}
