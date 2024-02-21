471f6c8bbd7d62bb15a941de2768650d68a5d36a
MattO <mattoc5102@gmail.com>
2024-02-21 17:06:20 +0000

    working version

--- app/src/main/java/com/example/scraps/FoodItemScreen.java
+++ app/src/main/java/com/example/scraps/FoodItemScreen.java
@@ -2,8 +2,10 @@ package com.example.scraps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.scraps.DBModels.FoodItem;
 
@@ -15,10 +17,19 @@ public class FoodItemScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_item_screen);
+
+        /*
         Intent intent = getIntent();
         FoodItem foodItem = (FoodItem) intent.getSerializableExtra("foodItem");
         // e.g.
         String foodName = foodItem.getName();
+        */
+
+        TextView Item = findViewById(R.id.item);
+        Item.setText("New String Value");
+
     }
+
+
 
 }
