6009b2f8258cc5fcd4cf32627163768ac0df0be4
D0van <jt2394@bath.ac.uk>
2024-02-21 19:57:42 +0000

    AddDays, CheckDay, CheckMonth added

--- app/src/main/java/com/example/scraps/IntDate.java
+++ app/src/main/java/com/example/scraps/IntDate.java
@@ -9,7 +9,7 @@ public class IntDate {
     public Integer Month(){return dateArray[1];}
     public Integer Year(){return dateArray[2];}
 
-    public IntDate(){ // Gets current date as an Integer array of [Day, Month, Year]. Year is the full year (i.e: 2024)
+    public IntDate(){
         Date date = new Date();
         DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.SHORT, Locale.FRANCE);
         dateArray = ConvertDate(dateFormatter.format(date));
@@ -40,7 +40,13 @@ public class IntDate {
 
     public String DateAsString(){
         StringBuilder sb = new StringBuilder();
-        sb.append(Day());
+        if (Day() >= 10){
+            sb.append(Day());
+        }
+        else{
+            sb.append('0');
+            sb.append(Day());
+        }
         sb.append('/');
         if (Month() >= 10){
             sb.append(Month());
@@ -52,6 +58,44 @@ public class IntDate {
         sb.append('/');
         sb.append(Year());
         return sb.toString();
+    }
+
+    public void AddDays(Integer numberOfDays){
+        if (numberOfDays >= 0){
+            dateArray[0] = Day() + numberOfDays;
+            CheckDay();
+        }
+    }
+
+    private void CheckDay(){
+        if (Day() > 28 && Month() == 2 && Year() % 4 != 0){ // if day is greater than 28, month is feb and not a leap year
+            dateArray[0] = Day() - 28;
+            dateArray[1] = Month() + 1;
+            CheckDay();
+        }
+        if (Day() > 29 && Month() == 2 && Year() % 4 == 0){ // case for leap year
+            dateArray[0] = Day() - 29;
+            dateArray[1] = Month() + 1;
+            CheckDay();
+        }
+        if (Day() > 30 && (Month() == 4 || Month() == 6 || Month() == 9 || Month() == 11)){ // 30 day months
+            dateArray[0] = Day() - 30;
+            dateArray[1] = Month() + 1;
+            CheckDay();
+        }
+        if (Day() > 31 && (Month() == 1 || Month() == 3 || Month() == 5 || Month() == 7 || Month() == 8 || Month() == 10 || Month() == 12)){ // 31 day months
+            dateArray[0] = Day() - 31;
+            dateArray[1] = Month() + 1;
+            CheckDay();
+        }
+        CheckMonth();
+    }
+
+    private void CheckMonth(){
+        if (Month() > 12){
+            dateArray[1] = 1;
+            dateArray[2] = Year()+1;
+        }
     }
 }
 
