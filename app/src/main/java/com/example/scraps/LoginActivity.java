af0051fc9381b46fa769a78cc65db7c741821eaa
kuter555 <ch.wahlen01@gmail.com>
2024-02-21 19:13:33 +0000

    Create page for login

--- app/src/main/java/com/example/scraps/LoginActivity.java
+++ app/src/main/java/com/example/scraps/LoginActivity.java
@@ -3,12 +3,27 @@ package com.example.scraps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.scraps.DBModels.Registration;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Example usage
+                //login() method;
+            }
+        });
     }
 }
