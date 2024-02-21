c8576ce819484b4d0de3acc6476af84e4df6b7cb
James Medley <81381425+jmed13@users.noreply.github.com>
2024-02-21 16:11:15 +0000

    food database items screen

---
+++ app/src/main/java/com/example/scraps/DBModels/FoodItem.java
@@ -0,0 +1,25 @@
+package com.example.scraps.DBModels;
+
+public class FoodItem {
+    private String name;
+    private String expiry;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(String description) {
+        this.expiry = description;
+    }
+
+    // Constructor, getters, and setters
+}
+
