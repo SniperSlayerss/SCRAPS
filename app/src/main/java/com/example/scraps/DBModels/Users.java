2d533a4f9bc475a14491e709f15a838323fa4bad
kuter555 <ch.wahlen01@gmail.com>
2024-02-21 17:00:24 +0000

    created password hash func

--- app/src/main/java/com/example/scraps/DBModels/Users.java
+++ app/src/main/java/com/example/scraps/DBModels/Users.java
@@ -1,4 +1,8 @@
 package com.example.scraps.DBModels;
+import java.security.MessageDigest;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 
 public class Users {
@@ -14,6 +18,32 @@ public class Users {
         this.password = password;
         this.foodItems = new HashMap<>();
 
+    }
+
+    public static String getSHA(String input) throws NoSuchAlgorithmException
+    {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        // digest() method called
+        return toHexString(md.digest(input.getBytes(StandardCharsets.UTF_8)));
+    }
+
+    public static String toHexString(byte[] hash)
+    {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, hash);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 64)
+        {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
     }
 
     public String getUsername() {
