fd77978e8323b0172d9d3ce461f1951abf88fcf0
tonyvarkey <tonyparayil.tp@gmail.com>
2024-02-23 01:30:08 +0000

    Toolbar working on HomeActivity

--- app/src/main/java/com/example/scraps/HomeActivity.java
+++ app/src/main/java/com/example/scraps/HomeActivity.java
@@ -4,17 +4,21 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toolbar;
+import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
-import com.example.scraps.DBModels.FoodItem;
-import com.example.scraps.DBModels.Users;
+
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,26 +26,51 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Random;
-
-public class HomeActivity extends AppCompatActivity {
-
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
+    private LinearLayout navigationMenuLayout;
+    NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        setSupportActionBar(findViewById(R.id.my_toolbar));
-        UpdateExpiryTextView();
+
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+        TextView title = findViewById(R.id.title);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.menu_home);
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HomeActivity.class); //v.context() lets you access current class
+                startActivity(intent);
+            }
+        });
+
+        rightIcon.setOnClickListener(new View.OnClickListener() {
+            //button to open menu
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(v.getContext(), "MENU TOGGLE CLICKED", Toast.LENGTH_SHORT).show();
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+
+
     }
+
+
 
     // Method to handle button click and open the settings activity
     public void openSettingsActivity(View view) {
@@ -83,82 +112,24 @@ public class HomeActivity extends AppCompatActivity {
         myRef.setValue("joe");
     }
 
-    /**
-     * Gets all food items attached to a user that are set to expire between the current day and a specified number of days in the future.
-     * NOTE: Encapsulate into Users?
-     * @param currentUser
-     * @param numberOfDays
-     * @return
-     */
-    private ArrayList<FoodItem> GetExpiringFoodItems(Users currentUser, Integer numberOfDays){
-        if (currentUser.getFoodItems().isEmpty()){
-            return new ArrayList<FoodItem>();
-        }
-        else{
-            DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.SHORT, Locale.FRANCE);
-            ArrayList<FoodItem> foodItems = new ArrayList((currentUser.getFoodItems()).values());
-            ArrayList<FoodItem> output = new ArrayList<>();
-            Date desiredDate = new Date();
-            int newDay = desiredDate.getDate() + numberOfDays.intValue();
-            desiredDate.setDate(newDay);
-            Date currentDate = new Date();
-            Boolean exceptionCaught = false;
-            for (FoodItem f : foodItems){
-                try{
-                    Date expiryDate = dateFormatter.parse(f.getExpiryDate());
-                    if (expiryDate.compareTo(desiredDate) <= 0 && expiryDate.compareTo(currentDate) >= 0){
-                        output.add(f);
-                    }
-                }
-                catch (ParseException e){
-                    exceptionCaught = true;
-                    break;
-                }
-            }
-            if (exceptionCaught){
-                return new ArrayList<FoodItem>();
-            }
-            else{
-                return output;
-            }
-        }
+    //Copy below into java files for toolbar functionality
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
 
+        if (itemID == R.id.menu_home) {
+            Intent intent = new Intent(this, HomeActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        else if (itemID == R.id.menu_food_item) {
+            Intent intent = new Intent(this, FoodItemScreen.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        else if (itemID == R.id.menu_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.END);
+        return true;
     }
-
-    /**
-     * Written weirdly for testing purposes, ideally the current user should be accessible from the activity but for now I'm using a test user defined in scope.
-     * Currently picks a random FoodItem from the array until I decide how I want to sort the items.
-     * @param currentUser
-     */
-    private void UpdateExpiryTextView(){
-        // TEST CODE
-        Users testUser = new Users("Mr. Test", "someone@example.com", "p@ssW0rd123", "2394");
-        testUser.TESTMETHOD(new FoodItem("Eggs", "27/02/2024", "23/02/2024", 3.50, "Testing food", false, "Mr. Test"));
-        testUser.TESTMETHOD(new FoodItem("Fish", "28/02/2024", "23/02/2024", 3.50, "Testing food", false, "Mr. Test"));
-        testUser.TESTMETHOD(new FoodItem("Milk", "29/02/2024", "23/02/2024", 3.50, "Testing food", false, "Mr. Test"));
-        testUser.TESTMETHOD(new FoodItem("Bread", "01/03/2024", "23/02/2024", 3.50, "Testing food", false, "Mr. Test"));
-        testUser.TESTMETHOD(new FoodItem("Canned Tuna", "02/03/2024", "23/02/2024", 3.50, "Testing food", false, "Mr. Test"));
-        // TEST CODE END
-        TextView expiryReminder = findViewById(R.id.expiryReminder);
-        ArrayList<FoodItem> expiring = GetExpiringFoodItems(testUser, 2);
-        if (expiring.isEmpty()){
-            expiryReminder.setText("Nothing expiring soon");
-        }
-        else{
-            Random rnd = new Random();
-            StringBuilder sb = new StringBuilder();
-            sb.append("Your '");
-            sb.append(expiring.get(rnd.nextInt(expiring.size())).getFoodName());
-            sb.append("' item is expiring on ");
-            Date date = new Date();
-            DateFormat dateFormatter = DateFormat.getDateInstance(DateFormat.LONG, Locale.ENGLISH);
-            sb.append(dateFormatter.format(date));
-            expiryReminder.setText(sb.toString());
-        }
-
-    }
-
-
-
-
 }
