6422d53d82f8ab65ae69a7d1d0168d2fe01abc04
MattO <mattoc5102@gmail.com>
2024-02-23 14:38:54 +0000

    toolbar item screen

--- app/src/main/java/com/example/scraps/FoodItemScreen.java
+++ app/src/main/java/com/example/scraps/FoodItemScreen.java
@@ -1,18 +1,60 @@
 package com.example.scraps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.app.AlertDialog;
 import com.example.scraps.DBModels.FoodItem;
+import com.google.android.material.navigation.NavigationView;
 
-public class FoodItemScreen extends AppCompatActivity {
-
+public class FoodItemScreen extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private LinearLayout navigationMenuLayout;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+        TextView title = findViewById(R.id.title);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.menu_home);
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HomeActivity.class); //v.context() lets you access current class
+                startActivity(intent);
+            }
+        });
+
+        rightIcon.setOnClickListener(new View.OnClickListener() {
+            //button to open menu
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(v.getContext(), "MENU TOGGLE CLICKED", Toast.LENGTH_SHORT).show();
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_item_screen);
 
@@ -27,7 +69,7 @@ public class FoodItemScreen extends AppCompatActivity {
 
         //Loads correct text for each section of UI
         TextView Item = findViewById(R.id.item);
-        String ItemName = "NO NAME ERROR";
+        String ItemName = "NO NAME";
         if (foodItem != null) {
             ItemName = foodItem.getFoodName();
         }
@@ -78,6 +120,24 @@ public class FoodItemScreen extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+
+        if (itemID == R.id.menu_home) {
+            //since we are already on home page, do nothing and let window close
+        }
+        else if (itemID == R.id.menu_food_item) {
+            Intent intent = new Intent(this, FoodItemScreen.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        else if (itemID == R.id.menu_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.END);
+        return true;
+    }
 
 
 }
