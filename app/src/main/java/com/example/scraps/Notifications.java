9230d8fa3661f413cf1a014ee2e90ccd26d1212e
tonyvarkey <tonyparayil.tp@gmail.com>
2024-02-27 14:38:05 +0000

    Notifications update 1

--- app/src/main/java/com/example/scraps/Notifications.java
+++ app/src/main/java/com/example/scraps/Notifications.java
@@ -1,17 +1,98 @@
 package com.example.scraps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class Notifications extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class Notifications extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private LinearLayout navigationMenuLayout;
+    NavigationView navigationView;
+
+    private TextView notifsToggleState;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications);
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+        TextView title = findViewById(R.id.title);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+        //navigationView.setCheckedItem(R.id.menu_settings);
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HomeActivity.class); //v.context() lets you access current class
+                startActivity(intent);
+            }
+        });
+
+        rightIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
+
+
+        SwitchCompat notifsToggle = findViewById(R.id.notifications_switch);
+        notifsToggleState = findViewById(R.id.on_off_label);
+
+        notifsToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    notifsToggleState.setText("ON");
+                }
+                else {
+                    notifsToggleState.setText("OFF");
+                }
+            }
+        });
+
+
+
     }
 
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+
+        if (itemID == R.id.menu_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+        } else if (itemID == R.id.menu_food_item) {
+            Intent intent = new Intent(this, FoodItemScreen.class); //v.context() lets you access current class
+            startActivity(intent);
+        } else if (itemID == R.id.menu_home) {
+            Intent intent = new Intent(this, HomeActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.END);
+        return true;
+    }
 }
