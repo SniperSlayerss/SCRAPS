bceb9557d4aea6653acee616ff6c270ee38c54db
MattO <mattoc5102@gmail.com>
2024-02-28 12:39:00 +0000

    added toolbar to add food item page

--- app/src/main/java/com/example/scraps/FoodInputActivity.java
+++ app/src/main/java/com/example/scraps/FoodInputActivity.java
@@ -2,27 +2,37 @@ package com.example.scraps;
 
 import android.app.Dialog;
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.DialogFragment;
 
 import com.example.scraps.DBModels.FoodItem;
 import com.example.scraps.DBModels.Users;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Calendar;
 
-public class FoodInputActivity extends AppCompatActivity {
+public class FoodInputActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    NavigationView navigationView;
 
     private EditText foodNameEditText, expiryDateEditText, purchaseDateEditText, priceEditText, typeEditText;
     private Button submitButton;
@@ -32,6 +42,35 @@ public class FoodInputActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_input);
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), HomeActivity.class); //v.context() lets you access current class
+                startActivity(intent);
+            }
+        });
+
+        rightIcon.setOnClickListener(new View.OnClickListener() {
+            //button to open menu
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(v.getContext(), "MENU TOGGLE CLICKED", Toast.LENGTH_SHORT).show();
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
 
         foodNameEditText = findViewById(R.id.food_name_editText);
         expiryDateEditText = findViewById(R.id.expiry_date_editText);
@@ -122,5 +161,26 @@ public class FoodInputActivity extends AppCompatActivity {
             // For example: (assuming expiryDateEditText is the corresponding EditText)
             // expiryDateEditText.setText(selectedDate);
         }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+
+        if (itemID == R.id.menu_food_item) {
+            Intent intent = new Intent(this, FoodDatabaseScreenActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        else if (itemID == R.id.menu_home) {
+            Intent intent = new Intent(this, HomeActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        else if (itemID == R.id.menu_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.END);
+        return true;
     }
 }
