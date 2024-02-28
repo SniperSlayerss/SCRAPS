e7ae48e1ed3cdfac4848f257e200a3647bd247cf
James Medley <81381425+jmed13@users.noreply.github.com>
2024-02-28 16:27:56 +0000

    notifications

--- app/src/main/java/com/example/scraps/FirebaseMessaging.java
+++ app/src/main/java/com/example/scraps/FirebaseMessaging.java
@@ -1,10 +1,22 @@
 package com.example.scraps;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
 public class FirebaseMessaging extends FirebaseMessagingService {
+
     @Override
-    public void onMessageRecieved(RemoteMessage remoteMessage) {
-        //handle notifaction here
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // Check if the message contains data payload.
+
     }
+
+
 }
