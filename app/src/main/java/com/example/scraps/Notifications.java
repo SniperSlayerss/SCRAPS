0952e28eed4724572adcb98a83f1e66a62f915bb
tonyvarkey <tonyparayil.tp@gmail.com>
2024-02-28 16:13:22 +0000

    Settings Fixed, Notifications, Privacy and Household Updated

--- app/src/main/java/com/example/scraps/Notifications.java
+++ app/src/main/java/com/example/scraps/Notifications.java
@@ -1,26 +1,26 @@
 package com.example.scraps;
 
-        import androidx.annotation.NonNull;
-        import androidx.appcompat.app.AppCompatActivity;
-        import androidx.appcompat.widget.SwitchCompat;
-        import androidx.appcompat.widget.Toolbar;
-        import androidx.core.view.GravityCompat;
-        import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-        import android.content.Intent;
-        import android.content.SharedPreferences;
-        import android.os.Bundle;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.widget.CompoundButton;
-        import android.widget.ImageView;
-        import android.widget.LinearLayout;
-        import android.widget.Switch;
-        import android.widget.TextView;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
 
-        import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-        import org.w3c.dom.Text;
+import org.w3c.dom.Text;
 
 public class Notifications extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawerLayout;
@@ -219,7 +219,7 @@ public class Notifications extends AppCompatActivity implements NavigationView.O
         if (itemID == R.id.menu_settings) {
             Intent intent = new Intent(this, SettingsActivity.class);
         } else if (itemID == R.id.menu_food_item) {
-            Intent intent = new Intent(this, FoodItemScreen.class); //v.context() lets you access current class
+            Intent intent = new Intent(this, FoodDatabaseScreenActivity.class); //v.context() lets you access current class
             startActivity(intent);
         } else if (itemID == R.id.menu_home) {
             Intent intent = new Intent(this, HomeActivity.class); //v.context() lets you access current class
