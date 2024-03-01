302b0fc4520f2c6f915ae3e60eeb0cd04cfee598
James Medley <81381425+jmed13@users.noreply.github.com>
2024-03-01 14:18:31 +0000

    notification receiver

--- app/src/main/java/com/example/scraps/HomeActivity.java
+++ app/src/main/java/com/example/scraps/HomeActivity.java
@@ -15,7 +15,6 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.appcompat.widget.Toolbar;
 
 
