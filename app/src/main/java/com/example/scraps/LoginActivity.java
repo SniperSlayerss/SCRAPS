2bc6f2988e8488f75b26b86c5942ff43fff48334
D0van <jt2394@bath.ac.uk>
2024-03-05 20:06:47 +0000

    Replaced the textView with a RecyclerView, and renamed UpdateExpiryTextView to UpdateExpiryList. Also cleaned up the home page a little

--- app/src/main/java/com/example/scraps/LoginActivity.java
+++ app/src/main/java/com/example/scraps/LoginActivity.java
@@ -20,7 +20,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.messaging.FirebaseMessaging;
+//import com.google.firebase.messaging.FirebaseMessaging;
 
 public class LoginActivity extends AppCompatActivity {
 
