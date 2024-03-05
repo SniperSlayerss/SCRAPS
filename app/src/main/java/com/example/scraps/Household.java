63aaaff1dcc0622ac6bbb9b0bf812701928f50a8
tonyvarkey <tonyparayil.tp@gmail.com>
2024-03-05 15:19:20 +0000

    Household page's toolbar functioning

--- app/src/main/java/com/example/scraps/Household.java
+++ app/src/main/java/com/example/scraps/Household.java
@@ -1,17 +1,69 @@
 package com.example.scraps;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Household extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class Household extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    NavigationView navigationView;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.household);
+
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        ImageView rightIcon = findViewById(R.id.right_icon);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        leftIcon.setOnClickListener(v -> {
+            Intent intent = new Intent(this, HomeActivity.class);
+            startActivity(intent);
+        });
+
+        rightIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.END);
+            }
+        });
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemID = item.getItemId();
+
+        if (itemID == R.id.menu_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        } else if (itemID == R.id.menu_food_item) {
+            Intent intent = new Intent(this, FoodDatabaseScreenActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        } else if (itemID == R.id.menu_home) {
+            Intent intent = new Intent(this, HomeActivity.class); //v.context() lets you access current class
+            startActivity(intent);
+        }
+        drawerLayout.closeDrawer(GravityCompat.END);
+        return true;
+    }
 }
